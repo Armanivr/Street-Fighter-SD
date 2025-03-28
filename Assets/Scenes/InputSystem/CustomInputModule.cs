90100861b78ef2fd06adfe04d2cd1abe1b8799be
Mich0165 <145758915+Mich0165@users.noreply.github.com>
2025-03-28 14:07:34 +0100

    De bug van de vorige commit gefixt, movement code ingezet en getest.
    Basis beweging links rechts en springen staat er nu in. Verdere tuning komt op een latere moment.

--- Assets/Scenes/InputSystem/CustomInputModule.cs
+++ Assets/Scenes/InputSystem/CustomInputModule.cs
@@ -4,29 +4,49 @@ using UnityEngine.InputSystem;
 public class CustomInputModule : MonoBehaviour
 {
 
-    private Rigidbody2D player1;
-    private Rigidbody2D player2;
+    public Rigidbody2D player1;
+    public Rigidbody2D player2;
     private PlayerInput playerInput;
 
 
 
     private void Awake()
     {
-        player1= GetComponent<Rigidbody2D>();
-        player2 = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
 
-        PlayerInputActions playerInputactions = new PlayerInputActions();
-        //playerInputActions.Player1.Enable();   ------------------------------  ( Deze werkt momenteel niet voor een reden die ik niet doorheb) 
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.Player1.Enable();
+        playerInputActions.Player2.Enable();
 
+        playerInputActions.Player1.Jump.performed += Jump;
+        playerInputActions.Player2.Jump.performed += Jump;
 
+        playerInputActions.Player1.Movement.performed += Movement_performed;
+        playerInputActions.Player2.Movement.performed += Movement_performed;
 
     }
-    public void Jump()
+
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        float speed = 5f;
+
+        if (player1 != null)
+            player1.linearVelocity = new Vector2(inputVector.x * speed, player1.linearVelocity.y);
+
+        if (player2 != null)
+            player2.linearVelocity = new Vector2(inputVector.x * speed, player2.linearVelocity.y);
+    }
+
+
+    public void Jump(InputAction.CallbackContext context)
     {
         Debug.Log("Jump!");
-        player1.AddForce(Vector2.up * 1, ForceMode2D.Impulse);
-        player2.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+        if (player1 != null)
+            player1.AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+
+        if (player2 != null)
+            player2.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
     }
 
     //public Vector2 LeftAnalog()
