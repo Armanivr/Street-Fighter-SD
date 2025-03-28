095d9a95de5d4538f2dc386dff008b7bebc4c357
Mich0165 <145758915+Mich0165@users.noreply.github.com>
2025-03-28 13:27:18 +0100

    Begin aan de player input.
    Springen zowel met de gamepad als K&M voor player 1 en 2 zijn voldaan op een begin level. Beweging links/rechts ingezet maar niet getest. Momenteel probleem met "PlayerInputActions" ( staat nu in een comment)

---
+++ Assets/Scenes/InputSystem/CustomInputModule.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CustomInputModule : MonoBehaviour
+{
+
+    private Rigidbody2D player1;
+    private Rigidbody2D player2;
+    private PlayerInput playerInput;
+
+
+
+    private void Awake()
+    {
+        player1= GetComponent<Rigidbody2D>();
+        player2 = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputactions = new PlayerInputActions();
+        //playerInputActions.Player1.Enable();
+
+
+
+    }
+    public void Jump()
+    {
+        Debug.Log("Jump!");
+        player1.AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+        player2.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+    }
+
+    //public Vector2 LeftAnalog()
+    //{
+    //    Vector2 inputDir = PlayerInputSystem.Player1.Movement.ReadValue<Vector2>();
+    //    //Debug.Log(inputDir);
+    //    return inputDir;
+    //}
+
+
+    //public void Update()
+    //{
+    //    if(CustomInputModule.instance.Jump())
+    //    {
+    //        Debug.Log("Jump");
+    //    }
+    //}
+}
